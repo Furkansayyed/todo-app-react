9ef560a75b41fe6856bbdd5757572b9fc8fb2088
Furkan Sayyed <sayyedfurkan115@gmail.com>
2023-11-09 12:35:58 +0530

    taking user input from form

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 
-export default function AddTodo() {
+export default function AddTodo(props) {
+    const [title, setTitle] = useState("");
+    const [desc, setdesc] = useState("");
+
+    const submit = () => {
+    }
     return (
-        <div className='container my-3'>
+        <div className='container my-3 form-control card shadow-lg'>
             <h3>Add a Todo</h3>
-            <form>
+            <form onSubmit={submit}>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Task Title</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <label htmlFor="task" className="form-label">Task Title</label>
+                    <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} className="form-control" id="task" />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Task Description</label>
-                    <input type="text" className="form-control" id="exampleInputPassword1" />
+                    <label htmlFor="desc" className="form-label">Task Description</label>
+                    <input type="text" value={desc} onChange={(e) => { setdesc(e.target.value) }} className="form-control" id="desc" />
                 </div>
-                <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                </div>
-                <button type="submit" className="btn btn-success btn-sm">Submit</button>
+
+                <button type="submit" className="btn btn-success btn-sm">Add</button>
             </form>
         </div>
     )
